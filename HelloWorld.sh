5fd222bbf5863a612077a2e1222222bb1bd11b39
Sirisha <sirisha.yenigandla@vitap.ac.in>
2020-08-08 16:38:03 +0000

    added print statemnet

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-This program counts till 5 and breaks
+This program counts till 5 and break
+s
 valid=true
 count=1
 while [ $valid ]
 do
 echo $count
 if [ $count -eq 5 ];
+	echo "count $count
+	"
 then
 break
 fi
